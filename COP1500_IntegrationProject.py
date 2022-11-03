81a9b46597ea7278570d2f791e4ea8d858b017c8
HBement99 <115734227+HBement99@users.noreply.github.com>
2022-11-03 19:42:47 -0400

    Add files via upload

--- COP1500_IntegrationProject.py
+++ COP1500_IntegrationProject.py
@@ -30,12 +30,12 @@ def main():
     print(100 - 50)  # Shows the use of the subtraction symbol
 
     # initial player statistics
-    hp = 100
+    health_points = 100
     stamina = 100
     # player statistics by choice
     stat_points = 15
     print("Dont exceed a total of 15 stat_point total among all your stats combined")
-    strength = int(input("Enter a value for strength that is less than or equal to 15"))
+    strength = int(input("Enter a value for strength that is less than or equal to 15: "))
     if not (strength >= stat_points or strength <= 0):
         print("You entered a valid strength value!")
 
@@ -44,16 +44,17 @@ def main():
     print('Here is an example of floor division in use: ', strength // 3)
     # This is floor division, it gives the result of division minus the remainder
 
-    vitality = int(input("Enter a value for vitality that is less than or equal to 15"))
+    vitality = int(input("Enter a value for vitality that is less than or equal to 15: "))
     if "vitality < stat_points and vitality > 0":
         print("you have entered a valid value for vitality")
 
     print("\n")
 
-    print("Here is an example of multiplication ", vitality * 5)
-    # This is an example of multiplication, and it gives the product of two variables multiplied by one another.
+    print("Here is an example of squaring or using an exponent of the number which was input ", vitality ** 5)
+    #  This is an example of an exponentiation, and it gives the product of two variables multiplied by
+    #  itself another.
 
-    intelligence = int(input("Enter a value for your intelligence that is less than or equal to 15"))
+    intelligence = int(input("Enter a value for your intelligence that is less than or equal to 15: "))
     if "intelligence < stat_points and intelligence > 0":
         print("you have a valid value of stat_points for intelligence")
 
@@ -67,7 +68,7 @@ def main():
 
     control = True
     while control:
-        ans: str = (input("You came to a fork in the road which way do you choose? Right/Left "))
+        ans: str = (input("You came to a fork in the road which way do you choose? Right/Left: "))
 
         if ans.lower() == "left":
             print("A random traveler comes and greets you")
@@ -75,7 +76,7 @@ def main():
             # Bypasses the rest of the functions and statement in the while and continues on to the rest of the program
         if ans.lower() == "right":
             print(name, "You come across a giant.")
-            ans: str = (input("Would you like to run away or fight?\n run away/fight"))
+            ans: str = (input("Would you like to run away or fight?\n run away/fight: "))
 
             if ans.lower() == "run":
                 print("you returned to the fork in the road.")
@@ -86,9 +87,9 @@ def main():
     print("\n")
 
     print("The traveler seems really nice, and decides to ask you a question")
-    control = True
+    control = True  # received help from Andrew and helped modify code
     while control:
-        ans: str = (input("You are offered a choice of weapons to buy, would you like a weapon?\n yes/no"))
+        ans: str = (input("You are offered a choice of weapons to buy, would you like a weapon?\n yes/no: "))
 
         if ans.lower() == "no":
             print("It was nice speaking with you, be safe on your travels.")
@@ -104,7 +105,8 @@ def main():
             for weapons in weapons:
                 print(weapons)  # prints a list of the items within the weapons variable
                 print(
-                    len(weapons))  # prints out how long each individual string is within the list when put into the "for loop".
+                    len(weapons))
+                # prints out how long each individual string is within the list when put into for loop.
 
             print("\n")
             control = True
@@ -148,8 +150,30 @@ def main():
                                                       "the way and have given you supplies.")
 
 
-def function1(game):  # Defines the function chosen which I am able to call later in the program.
+def function1(game):  # Defines the function chosen (game) which I am able to call later in the program.
     print("Hunter " + game)
+
+    h = 524  # received help from W3 Schools.
+    print(h > 511 or 814 < h)  # Returns True due to Var H being greater than 511 and less than 814.
+
+    print("\n")
+
+    h = 550
+    print(not (h > 500 or 575 < h))  # It would return True, but due to the not operator it switches it to False
+
+    print("\n")
+
+    h = 520
+    print(h > 510 and 530 < h)  # It would only return True if both statements in the print statement are True.
+
+    print("\n")
+
+    h = 5
+    b = 24
+    print(h != b)
+    #  this would print True because "var h" is not equal (!=) "var b"
+
+    print("\n")
 
 
 main()
