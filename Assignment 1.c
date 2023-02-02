5f89f3380a9efb883ef9feab8ea709707c79baba
Hunter Bement <115734227+HBement99@users.noreply.github.com>
2023-02-02 15:14:10 -0500

    Add files via upload
    2 separate programs built into a single program.

---
+++ Assignment 1.c
@@ -0,0 +1,62 @@
+// Program 1 is Created by Hunter Bement on 02/01/2023 - 10:54 PM
+
+//#include <stdio.h>
+
+//********************************************************************************************************************
+
+// Program 1
+// Used to Determine whether a student has a passing average of all their classes.
+
+/*
+int main() {
+    char name[30];
+    printf("What is your first name? \n");
+    scanf("%s", name);
+
+    int UID;
+    UID = 0;
+    printf("What is your student UID#: \n", UID);
+    scanf("%d", &UID);
+
+    float a, b, c, d, e, g, p;
+    printf("Please enter 5 of your grades with commas between each one: \n");
+    scanf("%f,%f,%f,%f,%f", &a, &b, &c, &d, &e);
+
+    g = a + b + c + d + e;
+    p = g / 5;
+
+    printf("\n");
+
+    printf("Hello %s", name);
+    printf(". I see your Student Id # is %d\n", UID);
+    printf("It seems that the average percentage of your current grades is %.2f %%\n", p);
+
+    if(p >= 70){
+        printf("You have a passing average score, Good Job!");
+    } else printf("You need to work harder in your classes");
+
+    return 0;
+}
+*/
+
+//********************************************************************************************************************
+
+// Program 2
+//This program is designed to see if the input year is a leap year not.
+
+/*int main() {
+    int year;
+
+    printf("Enter a year: \n");
+    scanf("%d", &year);
+
+    if (year % 400 == 0 || year % 4 == 0) {
+        printf("This year was a leap year");
+    }   else {
+        printf("This year is not a leap year");
+    }
+
+    return 0;
+}*/
+
+//*********************************************************************************************************************
