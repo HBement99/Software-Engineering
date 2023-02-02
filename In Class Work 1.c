470f7a9c33fa08dc5001eb963a971a0543b681a5
Hunter Bement <115734227+HBement99@users.noreply.github.com>
2023-02-02 15:15:01 -0500

    Add files via upload
    5 programs built into 1 single program.

---
+++ In Class Work 1.c
@@ -0,0 +1,101 @@
+//#include <stdio.h>
+
+//
+// Created by Hunter B. on 2/1/2023.
+//
+
+// Program 1
+// Needed to have an output of 2 integer values and 1 float (factorial)
+
+/*int main() {
+    int a, b, c, d;
+    a = 5, b = 10, c = 3, d = 2;
+
+    double e, quotient;
+    e = 25.2, quotient = (float) e / d;
+    //creates the output of e divided by d (12.6 / 2)
+
+    printf("%d * %d = %d\n", a, c, a * c);
+    printf("%d\n", b);
+    //shows the output of the integer values and problems
+
+    printf("%.1f", quotient);
+    //prints the factorial value output
+
+    return 0;
+
+    }*/
+
+
+    // Program 2
+    //(Program where user inputs 3 numbers, and then it outputs those numbers backwards.)
+
+    /*int main() {
+        int a = 0, b = 0 ,c = 0;
+
+        printf("Input first number: \n", a);
+        scanf("%d", &a);
+
+        printf("Input a second number: \n", b);
+        scanf("%d", &b);
+
+        printf("Input a third number: \n", c);
+        scanf("%d", &c);
+
+        printf("%d, %d, %d", c, b, a);
+        //Outputs the numbers which were input from first to last!
+
+        return 0;
+
+        }*/
+
+    // Program 3
+    // (Create a factorial of a number)
+    // (I had an integer variable into a float variable)
+
+    /*int main() {
+        int a = 10;
+        float b = a;
+
+        printf("%.1f", b);
+
+        return 0;
+    }*/
+
+    // Program 4
+
+    /*int main(){
+
+        int a, b;
+        a = 5;
+        a = 10;
+        // Switched the original variable "a" from 5 --> 10.
+        // Now when a variable is used it will output 10 instead of 5.
+
+        printf("%d\n", a);
+
+        b = 15;
+        b = a;
+        //I have it listed that the "b" variable is equal to 15 but then
+        //have it listed that the "b" variable equals the "a" variable.
+        //Variable "b" went from 15 --> 10 ("a" variable value)
+
+        printf("%d", a);
+
+        return 0;
+    }*/
+
+    // Program 5
+
+    /*int main() {
+
+        int a, b;
+        printf("input a number: \n");
+        scanf("%d",&a);
+
+        for (b = 1; 10 >= b; b++) {
+            printf("%d * %d = %d\n", a, b, a * b);
+
+    }
+    }*/
+
